8b71353e9870e6858b982090048ad6ba96401d76
ayaz-alijamali <ayazalijamali64@gmail.com>
2024-05-01 19:40:40 +0500

    add some Question

--- laptop.js
+++ laptop.js
@@ -1,3 +1,6 @@
+// Question  _____ 46
+
+
 // Define the Laptop object constructor
 function Laptop(make, model, year) {
     this.make = HP_ProBook;
@@ -15,3 +18,29 @@ let myLaptop = new Laptop("HP Pro Book", "G8", 2024);
 
 // Call the describe method to log information about the laptop
 myLaptop.describe(); // Output: This is a 2024 HP probook g8 laptop.
+
+
+// Question  ___ 47
+
+
+const laptops = [
+    { make: 'Apple', model: 'MacBook Pro', year: 2022 },
+    { make: 'Dell', model: 'XPS 15', year: 2023 },
+    { make: 'HP', model: 'Pro Book G8', year: 2024 }
+  ];
+  
+  const [firstLaptop, secondLaptop] = laptops;
+  
+  console.log(firstLaptop);
+  console.log(secondLaptop);
+  
+
+// Question ____ 48
+
+
+const pricesSet1 = [1200, 900, 1500, 1100];
+const pricesSet2 = [1300, 1000, 1400, 1250];
+
+const combinedPrices = [...pricesSet1, ...pricesSet2].sort((a, b) => a - b);
+
+console.log(combinedPrices);
